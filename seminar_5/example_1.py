cf32411660933c5ebef6e6d3b85c786513285038
PrNatali <79377175544@ya.ru>
2023-03-01 17:05:37 +0300

    ex2

--- seminar_5/example_1.py
+++ seminar_5/example_1.py
@@ -1,3 +1,11 @@
 # Напишите программу, которая на вход принимает два числа A и B,
 # и возводит число А в целую степень Bс помощью рекурсии.
 
+def exp (num, x):
+    if x == 0:
+        return 1
+    elif x == 1:
+        return num
+    else:
+        return num * exp(num, x - 1)
+print (exp(int(input("number: ")), int(input("exp: "))))
